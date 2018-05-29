354a7fbeecaa3ea973e91ed747423a53e9190d8d
villion-wong <wanglei5590@gmail.com>
2018-05-29 12:38:39 -0700

    add get and post transaction method

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -20,6 +24,17 @@ app.post('/mine', (req, res) => {
     console.log(`New block added: ${block.toString()}`);
     p2pServer.syncChains();
     res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
+
 });
 
 app.listen(HTTP_PORT, () => {
